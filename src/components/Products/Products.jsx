892fd80572f00b84e320ce6ceb1b3f7dc9291633
Victor Mantilla Colon <vmcolon@Victors-Air.home>
2021-02-17 09:31:32 -0500

    updating files with src components stripe dependencies

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product"
+
+const products = [
+    { id: 1, name: "Shoes", description: "Running Shoes.", price: "$200", image: "https://i.imgur.com/7pnXJ7X.jpg" },
+    { id: 2, name: "Macbook", description: "Apple Macbook.", price: "$1500", image: "https://i.imgur.com/a69Ld67.jpg" },
+];
+
+const Products = () => {
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>   
+                ))}
+            </Grid>
+        </main>
+    )
+    
+}
+
+export default Products;
